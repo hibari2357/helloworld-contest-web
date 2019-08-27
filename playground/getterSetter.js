5a77819608747ae68ce1adafe3fd04bcf8d7e773
daiki.oka1118 <hibari235711@gmail.com>
2019-08-27 12:48:40 +0900

    fix

---
+++ playground/getterSetter.js
@@ -0,0 +1,24 @@
+
+class NumberWrapper {
+    constructor(value) {
+        this._value = value;
+    }
+    // `_value`プロパティの値を返すgetter
+    // get value() {
+    //     console.log("getter");
+    //     return this._value;
+    // }
+    // // `_value`プロパティに値を代入するsetter
+    // set value(newValue) {
+    //     console.log("setter");
+    //     this._value = newValue;
+    // }
+}
+
+const numberWrapper = new NumberWrapper(1);
+// "getter"とコンソールに表示される
+console.log(numberWrapper._value); // => 1
+// "setter"とコンソールに表示される
+numberWrapper._value = 42;
+// "getter"とコンソールに表示される
+console.log(numberWrapper._value); // => 42
